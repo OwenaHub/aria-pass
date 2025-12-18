cc95afcd5fac6ed611648da61e1f462217d92360
Ernest Haruna <92730168+ernestharuna@users.noreply.github.com>
2025-12-18 16:17:51 +0100

    edited loader

--- app/components/skeletons/splash-screen.tsx
+++ app/components/skeletons/splash-screen.tsx
@@ -4,8 +4,8 @@ export default function SplashScreen() {
     return (
         <div className='p-5 h-screen mx-auto flex flex-col items-center justify-center bg-white animated fadeIn relative'>
             <div className="flex flex-col gap-3 mb-7 items-center">
-                <div className="text-center leading-3 flex flex-col gap-3 items-center">
-                    <LoaderCircle size={20} strokeWidth={2.5} className='animate-spin h-5 w-5 text-primary' />
+                <div className="text-center leading-3 flex flex-col gap-5 items-center">
+                    <LoaderCircle size={28} strokeWidth={1} className='animate-spin h-5 w-5 text-primary' />
 
                     <p className='text-md font-semibold tracking-tighter animate-pulse'>
                         AriaPass
